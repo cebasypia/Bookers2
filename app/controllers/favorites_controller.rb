39474f5e4e5be5415ac99d6c9aae074c171dfcb0
vagrant <vagrant@localhost.localdomain>
2020-05-17 14:04:15 +0000

    Add favorite

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,20 @@
+class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @favorite = current_user.favorites.create(book_id: params[:book_id])
+    redirect_back fallback_location: root_url
+  end
+
+  def destroy
+    @favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
+    @favorite.destroy
+    redirect_back fallback_location: root_url
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :book_id)
+  end
+end
