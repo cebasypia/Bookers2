aa7aa0b14a4445f9b541075648a158cb72002331
vagrant <vagrant@localhost.localdomain>
2020-05-17 13:55:29 +0000

    Associate Favorite with Book add already_favorite? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :books
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_books, through: :favorites, source: :book
+
   attachment :profile_image, destroy: false
 
-  # バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: { maximum: 20, minimum: 2 }
   validates :introduction, length: { maximum: 50 }
+
+  def already_favorite?(book)
+    favorites.exists?(book_id: book.id)
+  end
 end
