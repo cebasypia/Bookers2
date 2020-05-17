6193e0c0109af6943eb7525c9c23d9a0c369eba4
vagrant <vagrant@localhost.localdomain>
2020-05-17 13:52:43 +0000

    Change method name- baria_user to ensure_correct_user Delete dafault comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :baria_user, only: [:edit, :update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new # new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+    @book = Book.new
   end
 
   def index
-    @users = User.all # 一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
-    @book = Book.new # new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
-   end
+    @users = User.all
+    @book = Book.new
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -32,10 +32,9 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-  # url直接防止　メソッドを自己定義してbefore_actionで発動。
-  def baria_user
+  def ensure_correct_user
     unless params[:id].to_i == current_user.id
       redirect_to user_path(current_user)
-  end
+    end
   end
 end
