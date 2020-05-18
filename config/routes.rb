3acdcdc3afb105d53199cd81b36324926f2c6d76
vagrant <vagrant@localhost.localdomain>
2020-05-18 06:27:07 +0000

    #3 Remove auto-added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  get 'comments/destroy'
-  get 'book_comments/create'
-  get 'book_comments/destroy'
   devise_for :users
 
   resources :users, only: [:show, :index, :edit, :update]
