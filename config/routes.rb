ab62c728c938835a3d7eaaaa296b0420f9fa6128
vagrant <vagrant@localhost.localdomain>
2020-05-18 03:35:01 +0000

    #3 Add comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
   get 'book_comments/create'
   get 'book_comments/destroy'
   devise_for :users
@@ -6,6 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :index, :edit, :update]
   resources :books do
     resources :favorites, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
 
   root 'home#top'
