9fbaad6f23e4d108612f5ca4e0179d876ef137f0
vagrant <vagrant@localhost.localdomain>
2020-05-19 15:21:17 +0000

    #9 Modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show, :index, :edit, :update] do
-    get 'relationships/followings', as: 'followings'
-    get 'relationships/followers', as: 'followers'
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
   resources :books do
     resources :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :relationships, only: [:create, :destroy]
 
   root 'home#top'
   get 'home/about'
