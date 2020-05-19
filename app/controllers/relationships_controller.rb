bbc93223265134c55639e737ed38514c73b9f3a3
vagrant <vagrant@localhost.localdomain>
2020-05-19 15:14:55 +0000

    #9 Move relationship save/destroy to user model Remove the wasted Use user_id instead of follow_id

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,39 +1,22 @@
 class RelationshipsController < ApplicationController
-  before_action :set_user, only: [:create, :destroy]
+  before_action :set_user
 
   def create
-    following = current_user.follow(@user)
-    if following.save
-      flash[:success] = "フォローしました"
-      redirect_back fallback_location: root_url
-    else
-      # なんでflash.nowか？:alertである必要性も？
-      flash.now[:alert] = "フォローに失敗しました"
-      redirect_back fallback_location: root_url
-    end
+    current_user.follow(@user)
+    redirect_back fallback_location: root_url
   end
 
   def destroy
-    following = current_user.unfollow(@user)
-    if following.destroy
-      flash[:success] = "フォローを解除しました"
-      redirect_back fallback_location: root_url
-    else
-      flash[:alert] = "フォロー解除に失敗しました"
-      redirect_back fallback_location: root_url
-    end
+    current_user.unfollow(@user)
+    redirect_back fallback_location: root_url
   end
 
   def followings
-    @new_book = Book.new
-    @user = User.find(params[:user_id])
     @users = @user.followings
     render 'users/index'
   end
 
   def followers
-    @new_book = Book.new
-    @user = User.find(params[:user_id])
     @users = @user.followers
     render 'users/index'
   end
@@ -41,6 +24,6 @@ class RelationshipsController < ApplicationController
   private
 
   def set_user
-    @user = User.find(params[:follow_id])
+    @user = User.find(params[:user_id])
   end
 end
