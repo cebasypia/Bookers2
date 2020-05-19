acb8be2550a46dc0d70605cdfbc8371ccc4540d0
vagrant <vagrant@localhost.localdomain>
2020-05-19 06:03:40 +0000

    #7 Add 4 Associations and Add methods- about relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_books, through: :favorites, source: :book
   has_many :book_comments, dependent: :destroy
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
   attachment :profile_image, destroy: false
 
@@ -16,5 +20,20 @@ class User < ApplicationRecord
 
   def already_favorite?(book)
     favorites.exists?(book_id: book.id)
+  end
+
+  def follow(other_user)
+    unless self == other_user
+      relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    followings.include?(other_user)
   end
 end
