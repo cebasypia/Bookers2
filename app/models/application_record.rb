2def4153c8d80cf07e2e43eee1b94e620a478930
vagrant <vagrant@localhost.localdomain>
2020-05-20 12:42:34 +0000

    #14 Add a search method Rename search to str Add type param

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,18 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def self.search(column, search)
-    return all unless search
-    where(["#{column} LIKE ?", "%#{search}%"])
+  def self.search(column, str, type)
+    return all unless str
+    case type
+    when "forward"
+      where(["#{column} LIKE ?", "#{str}%"])
+    when "backward"
+      where(["#{column} LIKE ?", "%#{str}"])
+    when "partial"
+      where(["#{column} LIKE ?", "%#{str}%"])
+    when "exact"
+      where(["#{column} LIKE ?", "#{str}"])
+    else
+    end
   end
 end
