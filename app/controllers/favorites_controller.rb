7aa59addd02bdf2ccbcdb7bb48d39dcc1a2db5f0
vagrant <vagrant@localhost.localdomain>
2020-05-21 07:20:34 +0000

    #17 Change request html to js Refresh a part of books by create&destroy.js.erb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
 
   def create
     current_user.favorites.create(book_id: params[:book_id])
-    redirect_back fallback_location: root_url
+    @book = Book.find(params[:book_id])
   end
 
   def destroy
     favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
     favorite.destroy
-    redirect_back fallback_location: root_url
+    @book = Book.find(params[:book_id])
   end
 
   private
