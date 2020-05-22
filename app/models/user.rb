2e346cc166c079b06afbad18ae04c79211de68c2
vagrant <vagrant@localhost.localdomain>
2020-05-22 03:33:47 +0000

    #20 コピペ 何してるかよくわかってない postal_codeからprefecture_nameに変換するメソッドを用意します。

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,4 +38,12 @@ class User < ApplicationRecord
   def following?(other_user)
     followings.include?(other_user)
   end
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 end
