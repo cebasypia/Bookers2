351643015b13622330954058df519f58c87caa1c
vagrant <vagrant@localhost.localdomain>
2020-05-25 12:50:46 +0000

    Regenerate master.key Remake credentials.yml.enc Add refile secret key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a1c2f93a547659cd9227bfe25342f3d3ce2eb3e2dd3c57639323b2af0665502fc2382831df694e0f9135c50d503194f3c6a6aff60ce022c855b6bbbcc7edd833'
